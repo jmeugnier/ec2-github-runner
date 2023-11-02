f17b4a6e3a80ca049094c551d93b3116fcfa15cf
Julien MEUGNIER <julien.meugnier@kubapay.com>
2023-11-02 02:49:01 +0100

    Amelioration 1

--- src/aws.js
+++ src/aws.js
@@ -4,46 +4,65 @@ const config = require('./config');
 
 // User data scripts are run as the root user
 function buildUserDataScript(githubRegistrationToken, label) {
+
+  const userData = [
+    '#!/bin/bash'
+  ];
+
   if (config.input.runnerHomeDir) {
     // If runner home directory is specified, we expect the actions-runner software (and dependencies)
     // to be pre-installed in the AMI, so we simply cd into that directory and then start the runner
-    return [
-      '#!/bin/bash',
+    userData.push(...[
+      'echo "=== RUN ec2-github-runner INIT SCRIPT (Existing github-runner Mode) ==="',
       `cd "${config.input.runnerHomeDir}"`,
-      'export HOME=$(pwd)',
-      'export DOTNET_CLI_HOME=$HOME',
-      `echo "${config.input.preRunnerScript}" > pre-runner-script.sh`,
-      'source pre-runner-script.sh',
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-      './run.sh',
-    ];
-  } else {
-    return [
-      '#!/bin/bash',
+      'echo "[ec2-github-runner] go to actions-runner folder: $(pwd)"'
+    ]);
+  }
+  else {
+    userData.push(...[
+      'echo "=== RUN ec2-github-runner INIT SCRIPT (Install Mode) ==="',
       'mkdir actions-runner && cd actions-runner',
-      'echo "=== RUN ec2-github-runner INIT SCRIPT ==="',
-      'echo "[ec2-github-runner] actions-runner folder location: $(pwd)"',
-      `echo "${config.input.preRunnerScript}" > pre-runner-script.sh`,
-      'echo "[ec2-github-runner] RUN $(pwd)/pre-runner-script.sh"',
-      'source pre-runner-script.sh',
+      'echo "[ec2-github-runner] create and go to actions-runner folder: $(pwd)"'
+    ]);
+  }
+
+  userData.push(...[
+    `echo "${config.input.preRunnerScript}" > pre-runner-script.sh`,
+    'echo "[ec2-github-runner] RUN $(pwd)/pre-runner-script.sh"',
+    'source pre-runner-script.sh',
+    'export RUNNER_ALLOW_RUNASROOT=1',
+    'export HOME=$(pwd)',
+    'export DOTNET_CLI_HOME=/tmp',
+    'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
+  ]);
+
+  if (config.input.runnerHomeDir) {
+    userData.push(...[
+      'echo "[ec2-github-runner] Runner Environment Variables:"',
+      'env'
+    ]);
+  }
+  else {
+    userData.push(...[
       `export RUNNER_VERSION=${config.input.runnerVersion}`,
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      'export HOME=$(pwd)',
-      'export DOTNET_CLI_HOME=/tmp',
-      'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
       'echo "[ec2-github-runner] Runner Environment Variables:"',
       'env',
       'echo "[ec2-github-runner] Download https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-${RUNNER_ARCH}-${RUNNER_VERSION}.tar.gz..."',
       'curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-${RUNNER_ARCH}-${RUNNER_VERSION}.tar.gz',
-      'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-${RUNNER_VERSION}.tar.gz',
-      'echo "[ec2-github-runner] Run GitHub Action $(pwd)/config.sh..."',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-      'echo "[ec2-github-runner] === END INIT SCRIPT: Run GitHub Action $(pwd)/run.sh ==="',
-      './run.sh',
-    ];
+      'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-${RUNNER_VERSION}.tar.gz'
+    ]);
   }
+
+  userData.push(...[
+    'echo "[ec2-github-runner] Run GitHub Action $(pwd)/config.sh..."',
+    `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
+    'echo "[ec2-github-runner] === END INIT SCRIPT: Run GitHub Action $(pwd)/run.sh ==="',
+    './run.sh'
+  ]);
+
+  return userData;
 }
+
 
 async function startEc2Instance(label, githubRegistrationToken) {
   const ec2 = new AWS.EC2();
